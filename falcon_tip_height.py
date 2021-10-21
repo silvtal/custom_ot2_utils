e7439ee44bac165da46c80c3922493ce6a4bed4a
Silvia Talavera Marcos <stalaveramarcos@gmail.com>
2021-10-21 11:32:56 +0200

    Create falcon_tip_height.py

---
+++ falcon_tip_height.py
@@ -0,0 +1,26 @@
+def falcon_tip_height(vol, min_height = 0.5, r = 15/2, depth = 20):
+    """
+    Tracking volume function designed for Fisherbrand 
+    15 mL Falcon tubes.
+    
+    vol is in mL
+    r is the radius in mm
+    min_height is the minimum height in mm
+    depth is how deep you want the tip to enter the liquid (mm)
+    """
+
+    if (vol < 2):
+        return min_height
+
+    else:        
+        base = 27 # the height in mm from 0 to 2 mL is different
+        # (that's why we substract 2 here:)
+        vol2 = (vol - 2) * 1000 # from mL to mm**3
+
+        # now we do V = h * pi * r**2    (plus the base height)
+        height = base + vol2/(3.14159*((r)**2)) - depth
+
+        if ((height >= min_height) or (vol < 2)):
+            return height
+        else:
+            return min_height
